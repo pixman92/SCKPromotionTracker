c64f9aa75ee4aaa95d29eed536f989aa5dc5a361
Macbook <macbook@Macbooks-MacBook-Pro-2.local>
2023-07-05 16:54:40 -0700

    working on it

---
+++ script.js
@@ -0,0 +1,8 @@
+let singleStudent = {};s
+function makeOneStudentObj(name, age, beltRank, ){
+    singleStudent['name'] = name;
+    singleStudent['age'] = age;
+    singleStudent['beltRank'] = beltRank;
+}
+
+makeOneStudentObj('sam', '12', ['whiteYellow', 'brown'])
